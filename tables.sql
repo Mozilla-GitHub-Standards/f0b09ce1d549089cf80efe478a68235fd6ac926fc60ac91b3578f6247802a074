73e9bd84f2331640ca07eb877c6bf6c76f41ba1e
Andreas Wagner <mail@andreaswagner.org>
2016-10-12 14:30:43 +0200

    Update SQL script

--- tables.sql
+++ tables.sql
@@ -1,13 +1,14 @@
-CREATE TABLE IF NOT EXISTS addons (
+CREATE TABLE IF NOT EXISTS test.addons (
     id SERIAL PRIMARY KEY,
     url VARCHAR(256) UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS results (
+CREATE TABLE IF NOT EXISTS test.results (
     id SERIAL PRIMARY KEY,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     addon_id INT REFERENCES addons (id) NOT NULL,
     version TEXT NOT NULL,
     compatible BOOLEAN NOT NULL,
-    comment TEXT
+    comment TEXT,
+    session VARCHAR(32)
 );
