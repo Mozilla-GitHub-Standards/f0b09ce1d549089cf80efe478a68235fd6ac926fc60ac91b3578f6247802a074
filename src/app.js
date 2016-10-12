9805ad62e07cc804f4a3690e6fbc63b00c40ef2f
Andreas Wagner <mail@andreaswagner.org>
2016-10-12 14:01:18 +0200

    Add user sessions

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
 const lessMiddleware = require('less-middleware');
+
 const Addon = require('./models/addon.js');
 const AddonReport = require('./models/addonReport.js');
+
 require('source-map-support').install();
 
 const app = express();
@@ -15,10 +18,16 @@ app.set('views', './bin/views');
 app.use(lessMiddleware(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 const wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 app.get('/', wrap(async(req, res) => {
+    let cookies = req.cookies;
+    if (!cookies || !cookies.s) {
+        let opts = { maxAge : 2592000000 };
+        res.cookie('s', Math.random().toString(36).slice(2), opts);
+    }
     try {
         let addonUrl = await Addon.getLeastTestedAddon();
         res.render('index', {
@@ -45,7 +54,9 @@ app.post('/', wrap(async(req, res) => {
                     });
                     return;
                 } else {
-                    await new AddonReport(addonUrl, compatible, comment).save();
+                    let cookies = req.cookies;
+                    let session = (cookies && cookies.s) ? cookies.s : 0;
+                    await new AddonReport(addonUrl, compatible, comment, session).save();
                 }
             }
         }
--- src/models/addonReport.js
+++ src/models/addonReport.js
@@ -2,10 +2,11 @@ const dbPool = require('./db');
 const Addon = require('./addon.js');
 
 class AddonReport extends Addon {
-    constructor(url, is_compatible, comment) {
+    constructor(url, is_compatible, comment, session) {
         super(url);
         this.is_compatible = is_compatible;
         this.comment = comment;
+        this.session = session;
     }
 
     async save() {
@@ -16,9 +17,9 @@ class AddonReport extends Addon {
         if (result && result.id) {
             let version = await this.getCurrentVersion();
             dbPool.query(
-                `INSERT INTO results (addon_id, version, compatible, comment)
-                 VALUES ($1, $2, $3, $4);`,
-                [parseInt(result.id, 10), version, this.is_compatible, this.comment]
+                `INSERT INTO test.results (addon_id, version, compatible, comment, session)
+                 VALUES ($1, $2, $3, $4, $5);`,
+                [parseInt(result.id, 10), version, this.is_compatible, this.comment, this.session]
             );
         }
     }
